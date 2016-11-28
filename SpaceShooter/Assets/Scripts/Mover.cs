a8c650463b8fd5636f44d8fb062fd05348051a57
danielcy <448219947@qq.com>
2016-11-28 18:48:21 +0800

    Creating shot.

---
+++ SpaceShooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+
+	private Rigidbody rigidbody;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
